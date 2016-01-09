b9ab0b35a56c2051723e753d35c930a47d400df4
Jim Riordan <jim@j1mr10rd4n.info>
2016-01-09 17:24:22 +1000

    Compose planet-monitor text in function

--- submissions/j1mr10rd4n/src/ui_of_the_sith/core.cljs
+++ submissions/j1mr10rd4n/src/ui_of_the_sith/core.cljs
@@ -3,11 +3,14 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(defn planet-monitor-text
+  [planet]
+  (str "Obi-Wan currently on " planet))
 
 (defui PlanetMonitor
   Object
   (render [this]
-    (dom/h1 nil "Obi-Wan currently on Earth")))
+    (dom/h1 nil (planet-monitor-text "Earth"))))
 
 (def planet-monitor (om/factory PlanetMonitor))
 
