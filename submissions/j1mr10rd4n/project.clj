3f35091a17b425ea373eff841c7f05ac3d465213
Jim Riordan <jim@j1mr10rd4n.info>
2016-01-20 14:06:37 +1000

    use om-1.0.0-alpha23 to get query->ast method

--- submissions/j1mr10rd4n/project.clj
+++ submissions/j1mr10rd4n/project.clj
@@ -2,6 +2,6 @@
   :description "Flux Challenge - Sith Database UI"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.omcljs/om "1.0.0-alpha22"]
+                 [org.omcljs/om "1.0.0-alpha23"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
 
