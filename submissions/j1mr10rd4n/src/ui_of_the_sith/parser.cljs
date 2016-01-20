483fc62951bda8ab3da85a3b782a142da1205dc0
Jim Riordan <jim@j1mr10rd4n.info>
2016-01-21 09:45:04 +1000

    add new jedis to list linking them through the master/apprentice chain

--- submissions/j1mr10rd4n/src/ui_of_the_sith/parser.cljs
+++ submissions/j1mr10rd4n/src/ui_of_the_sith/parser.cljs
@@ -24,17 +24,38 @@
     {:value {:keys [:obi-wan-planet]}
      :action #(swap! state assoc :obi-wan-planet obi-wan-planet) })
 
+(defn create-master-of
+  [dark-jedi]
+    {:id (dark-jedi :master-id)
+     :name (str "Master of " (dark-jedi :name))
+     :homeworld "unknown"
+     :master-id (om/tempid)
+     :apprentice-id (dark-jedi :id)})
+
+(defn create-apprentice-of
+  [dark-jedi]
+    {:id (dark-jedi :apprentice-id)
+     :name (str "Apprentice of " (dark-jedi :name))
+     :homeworld "unknown"
+     :master-id (dark-jedi :id)
+     :apprentice-id (om/tempid)})
+
 (defmethod mutate 'dark-jedis/scroll
   [{:keys [state] :as env} key {:keys [direction] :as params}]
-  (.log js/console "dark-jedis/list: " (str (@state :dark-jedis/list)))
     (condp = direction
-      "down" (let [new-jedi-4 {:id 1 :name "steve" :homeworld "mars"}
-                   new-jedi-5 {:id 2 :name "bob" :homeworld "jupiter"}
-                   jedi-array (apply conj (subvec (@state :dark-jedis/list) 2 5) [new-jedi-4 new-jedi-5])]
+      "down" (let [dark-jedis-old (@state :dark-jedis/list)
+                   new-jedi-4 (create-apprentice-of (dark-jedis-old 4))
+                   new-jedi-5 (create-apprentice-of new-jedi-4)
+                   jedi-array (apply conj 
+                                     (subvec dark-jedis-old 2 5)
+                                     [new-jedi-4 new-jedi-5])]
         {:value {:keys [:dark-jedis/list]}
          :action #(swap! ui-of-the-sith.core/app-state assoc :dark-jedis/list jedi-array)})
-      "up" (let [new-jedi-1 {:id 1 :name "jon" :homeworld "mars"}
-                 new-jedi-2 {:id 2 :name "jim" :homeworld "jupiter"}
-                 jedi-array (apply conj [new-jedi-1 new-jedi-2] (subvec (@state :dark-jedis/list) 0 3))]
+      "up" (let [dark-jedis-old (@state :dark-jedis/list)
+                 new-jedi-2 (create-master-of (dark-jedis-old 0))
+                 new-jedi-1 (create-master-of new-jedi-2)
+                 jedi-array (apply conj
+                                   [new-jedi-1 new-jedi-2]
+                                   (subvec (@state :dark-jedis/list) 0 3))]
         {:value {:keys [:dark-jedis/list]}
          :action #(swap! ui-of-the-sith.core/app-state assoc :dark-jedis/list jedi-array)})))
