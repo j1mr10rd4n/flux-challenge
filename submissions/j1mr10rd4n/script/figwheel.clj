cdd0649e9f1795e923cb7989672d037e8bdd3f23
Jim Riordan <jim@j1mr10rd4n.info>
2016-02-09 20:30:23 +1000

    Add build for minified javascript version

--- submissions/j1mr10rd4n/script/figwheel.clj
+++ submissions/j1mr10rd4n/script/figwheel.clj
@@ -3,7 +3,7 @@
 
 (ra/start-figwheel!
   {:figwheel-options {}
-   :build-ids ["dev"]
+   :build-ids ["dev" "min"]
    :all-builds
    [{:id "dev"
      :figwheel true
@@ -12,6 +12,16 @@
                 :asset-path "js"
                 :output-to "resources/public/js/main.js"
                 :output-dir "resources/public/js"
-                :verbose true}}]})
+                :verbose true
+                :source-map true
+                :source-map-timestamp true}}
+    {:id "min"
+     :figwheel false
+     :source-paths ["src"]
+     :compiler {:main `ui-of-the-sith.core
+                :asset-path "js"
+                :output-to "app/js/main.js"
+                :output-dir "app/js"
+                :optimizations :advanced}}]})
 
 (ra/cljs-repl)
